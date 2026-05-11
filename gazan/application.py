5dce1a48a7f7e01dd8ab5e9320daa1324d1c358c
Subhan Gadirli <subhan.gadirli.en@student.aztu.edu.az>
2026-05-11 21:01:12 +0400

    add about page

--- gazan/application.py
+++ gazan/application.py
@@ -1,15 +1,17 @@
 import sys
+from pathlib import Path
 
 import gi
 
 gi.require_version("Gtk", "4.0")
 gi.require_version("Adw", "1")
 
-from gi.repository import Adw, Gio  # noqa: E402
+from gi.repository import Adw, Gdk, Gio, Gtk  # noqa: E402
 
 from gazan.ui.window import GazanWindow  # noqa: E402
 
 APP_ID = "io.codeberg.subhagadirli.Gazan"
+_ASSETS_DIR = Path(__file__).parent / "assets"
 
 
 class GazanApplication(Adw.Application):
@@ -18,6 +20,13 @@ class GazanApplication(Adw.Application):
             application_id=APP_ID,
             flags=Gio.ApplicationFlags.DEFAULT_FLAGS,
         )
+
+    def do_startup(self) -> None:
+        Adw.Application.do_startup(self)
+        display = Gdk.Display.get_default()
+        if display is not None:
+            theme = Gtk.IconTheme.get_for_display(display)
+            theme.add_search_path(str(_ASSETS_DIR))
 
     def do_activate(self) -> None:
         window = self.props.active_window
--- gazan/ui/window.py
+++ gazan/ui/window.py
@@ -3,10 +3,13 @@ import gi
 gi.require_version("Gtk", "4.0")
 gi.require_version("Adw", "1")
 
-from gi.repository import Adw, Gtk  # noqa: E402
+from gi.repository import Adw, Gio, Gtk  # noqa: E402
 
 from gazan.ui.add_remote_dialog import AddRemoteDialog  # noqa: E402
 from gazan.ui.remotes_page import RemotesPage  # noqa: E402
+
+APP_VERSION = "0.1.1"
+APP_ID = "io.codeberg.subhagadirli.Gazan"
 
 
 class GazanWindow(Adw.ApplicationWindow):
@@ -16,6 +19,8 @@ class GazanWindow(Adw.ApplicationWindow):
         self.set_title("Gazan")
         self.set_default_size(900, 600)
 
+        self._setup_actions()
+
         self._toast_overlay = Adw.ToastOverlay()
         self.set_content(self._toast_overlay)
 
@@ -23,10 +28,21 @@ class GazanWindow(Adw.ApplicationWindow):
         self._toast_overlay.set_child(toolbar_view)
 
         header = Adw.HeaderBar()
+
         add_button = Gtk.Button(icon_name="list-add-symbolic")
         add_button.set_tooltip_text("Add cloud storage")
         add_button.connect("clicked", lambda _b: self._open_add_remote_dialog())
         header.pack_end(add_button)
+
+        menu = Gio.Menu()
+        menu.append("About Gazan", "win.about")
+        menu_button = Gtk.MenuButton(
+            icon_name="open-menu-symbolic",
+            menu_model=menu,
+            tooltip_text="Main Menu",
+        )
+        header.pack_end(menu_button)
+
         toolbar_view.add_top_bar(header)
 
         self._remotes_page = RemotesPage(
@@ -34,6 +50,26 @@ class GazanWindow(Adw.ApplicationWindow):
             on_status_message=self._show_status,
         )
         toolbar_view.set_content(self._remotes_page)
+
+    def _setup_actions(self) -> None:
+        about_action = Gio.SimpleAction.new("about", None)
+        about_action.connect("activate", self._show_about)
+        self.add_action(about_action)
+
+    def _show_about(self, _action, _param) -> None:
+        dialog = Adw.AboutDialog(
+            application_name="Gazan",
+            application_icon=APP_ID,
+            developer_name="Subhan Gadirli",
+            version=APP_VERSION,
+            website="https://codeberg.org/subhangadirli/gazan",
+            issue_url="https://codeberg.org/subhangadirli/gazan/issues",
+            license_type=Gtk.License.GPL_3_0,
+            copyright="© 2024 Subhan Gadirli",
+            comments="A graphical frontend for rclone",
+            developers=["Subhan Gadirli https://codeberg.org/subhangadirli"],
+        )
+        dialog.present(self)
 
     def _open_add_remote_dialog(self) -> None:
         existing = {r["name"] for r in self._remotes_page.remotes}
